cd53d47284de2486291b2504101ca788b7558da5
IcedJavaRobotics <IcedJavaRobotics@gmail.com>
2026-02-05 19:40:43 -0500

    setup slave/master motor for shooter

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,9 +4,15 @@
 
 package frc.robot.subsystems;
 
+import java.util.Map;
+
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,7 +25,15 @@ public class ShooterSubsystem extends SubsystemBase {
   TalonFX shooterMotorSlave;
 
   ShuffleboardTab functionsCheckTab = Shuffleboard.getTab("Functions Check");
-  
+
+//  public final NetworkTableEntry myValue =
+//       functionsCheckTab.add("My Value", 0.0)
+//          .withWidget("Number Slider")
+//          .withProperties(Map.of(
+//              "min", 0,
+//              "max", 1
+//          ))
+//          .getEntry();
 
   PIDController spinUpController = new PIDController(0.1, 0, 0);
 
@@ -28,6 +42,7 @@ public class ShooterSubsystem extends SubsystemBase {
     this.shooterMotorMaster = new TalonFX(HardwareConstants.SHOOTER_LEFT_MOTOR_ID, HardwareConstants.CANBUS);
     this.shooterMotorSlave = new TalonFX(HardwareConstants.SHOOTER_LEFT_MOTOR_ID, HardwareConstants.CANBUS);
 
+    shooterMotorSlave.setControl(new Follower(HardwareConstants.SHOOTER_LEFT_MOTOR_ID, MotorAlignmentValue.Opposed)); //sets the slave motor to follow the master motor movement
   }
 
   public void spinUp(double speed){
